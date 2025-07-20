78614a0268aba35365ad305c5761124591ed0a00
Chris Jewell <chrisjohnjewell@gmail.com>
2025-07-20 10:36:22 +1000

    Fix PR #2 feedback: new tasks start as not done, remove unused file, add DB constraints (#4)
    * Fix PR #2 feedback: new tasks start as not done, remove unused file, add DB constraints
    
    * Fix RuboCop linting issues: array bracket spacing and empty lines

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,12 +1,4 @@
 class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :interval_type, presence: true, inclusion: { in: %w[daily weekly monthly] }
-
-  before_create :set_initial_completion_time
-
-  private
-
-  def set_initial_completion_time
-    self.last_completed_at ||= Time.current
-  end
 end
